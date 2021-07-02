23bed2aeefd612ed1690fd408a456637249f2997
Giraphi <hoeps.raphael@gmail.com>
2021-07-02 09:48:27 +0200

    remove focus style

--- src/components/three-canvas.tsx
+++ src/components/three-canvas.tsx
@@ -2,16 +2,22 @@ import React, {useContext} from "react";
 import {Canvas} from "@react-three/fiber";
 import MovementContext from "../contexts/movement-context";
 import CanvasContent from "./canvas-content";
+import styled from "styled-components";
 
+const StyledCanvas = styled(Canvas)`
+    canvas:focus {
+        outline: none;
+    }
+`
 
 export default function ThreeCanvas() {
     const movementContext = useContext(MovementContext);
 
     return (
-        <Canvas>
+        <StyledCanvas>
             <MovementContext.Provider value={movementContext}>
                 <CanvasContent/>
             </MovementContext.Provider>
-        </Canvas>
+        </StyledCanvas>
     );
 }
