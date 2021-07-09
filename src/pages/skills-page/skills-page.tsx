d1fb47be28bd1e1e8f1fe9087c8eeb74f165e225
Giraphi <hoeps.raphael@gmail.com>
2021-07-09 02:28:37 +0200

    background image

--- src/pages/skills-page/skills-page.tsx
+++ src/pages/skills-page/skills-page.tsx
@@ -1,11 +1,22 @@
 import React from "react";
 import {Link} from "react-router-dom";
+import styled from "styled-components";
+import Billelis from '../../images/Billelis.jpg';
+
+
+const StyledRoot = styled.div`
+    min-height: 100vh;
+    background-image: url(${Billelis});
+    background-size: cover;
+    background-position: center;
+    overflow:auto;
+`
 
 export default function SkillsPage() {
     return (
-        <>
+        <StyledRoot>
             <h1>Other Page</h1>
             <Link to="/">Back to home</Link>
-        </>
+        </StyledRoot>
     );
 }
