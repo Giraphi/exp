b875ab0872cfa523c319312cbd494840f3170948
Giraphi <hoeps.raphael@gmail.com>
2021-07-09 02:31:03 +0200

    remove background image

--- src/pages/skills-page/skills-page.tsx
+++ src/pages/skills-page/skills-page.tsx
@@ -1,14 +1,9 @@
 import React from "react";
 import {Link} from "react-router-dom";
 import styled from "styled-components";
-import Billelis from '../../images/Billelis.jpg';
-
 
 const StyledRoot = styled.div`
     min-height: 100vh;
-    background-image: url(${Billelis});
-    background-size: cover;
-    background-position: center;
     overflow:auto;
 `
 
