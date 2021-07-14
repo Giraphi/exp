e8bad56b2bf2bb82d4a57ebbf9a05fcb8fdee4a3
Giraphi <hoeps.raphael@gmail.com>
2021-07-14 12:04:51 +0200

    experiment with obj model

--- src/components/skills-page/skills-page-world.tsx
+++ src/components/skills-page/skills-page-world.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {Color, MeshNormalMaterial, MeshStandardMaterial} from "three";
+import {MeshStandardMaterial} from "three";
 import useWindowWidth from "../../hooks/use-window-width";
 import ObjToPrimitive from "../../ObjToPrimitive";
 
@@ -9,48 +9,74 @@ export default function SkillsPageWorld() {
 
     const mat = new MeshStandardMaterial(
         {
-            emissive: new Color("#ffffee"),
-            emissiveIntensity: 1,
-            color: "#000000",
+            color: "white",
         }
     )
 
     return (
         <>
-            {/*<ambientLight color="white" intensity={0.5}/>*/}
-
-            <pointLight
-                color={"#ffffff"}
-                intensity={1}
-                distance={500}
-                decay={2}
-                position={[- windowWidth/5,200,0]}
-                castShadow={true}
+            {/*<ambientLight color="white" intensity={0.05}/>*/}
+            <group
+                position={[-windowWidth / 5, 200, 0]}
             >
-                <mesh
-                    position={[0,0,0]}
+
+
+                {/*<pointLight*/}
+                {/*    color={"#ffffff"}*/}
+                {/*    intensity={3}*/}
+                {/*    distance={1000}*/}
+                {/*    decay={1}*/}
+                {/*    position={[-30, 0, 0]}*/}
+                {/*    castShadow={true}>*/}
+
+                {/*    <mesh*/}
+                {/*        position={[0, 0, 0]}*/}
+                {/*    >*/}
+                {/*        <meshStandardMaterial*/}
+                {/*            // args={[{emissive: "0xffffee", emissiveIntensity: 10, color: "0x000000"}]}*/}
+                {/*            emissive={new Color("#ffffee")}*/}
+                {/*            emissiveIntensity={1}*/}
+                {/*            color={"#000000"}*/}
+
+                {/*        />*/}
+                {/*        <sphereGeometry args={[10, 16, 8]}/>*/}
+                {/*    </mesh>*/}
+                {/*</pointLight>*/}
+
+
+                <pointLight
+                    color={"#ffffff"}
+                    intensity={3}
+                    distance={1000}
+                    decay={10}
+                    position={[40, 20, 40]}
+                    castShadow={true}
                 >
-                    <meshStandardMaterial
-                        // args={[{emissive: "0xffffee", emissiveIntensity: 10, color: "0x000000"}]}
-                        emissive={new Color("#ffffee")}
-                        emissiveIntensity={1}
-                        color={"#000000"}
+                    {/*<mesh*/}
+                    {/*    position={[0, 0, 0]}*/}
+                    {/*>*/}
+                    {/*    <meshStandardMaterial*/}
+                    {/*        // args={[{emissive: "0xffffee", emissiveIntensity: 10, color: "0x000000"}]}*/}
+                    {/*        emissive={new Color("#ffffee")}*/}
+                    {/*        emissiveIntensity={1}*/}
+                    {/*        color={"#000000"}*/}
 
-                    />
-                    <sphereGeometry args={[10, 16, 8]}/>
+                    {/*    />*/}
+                    {/*    <sphereGeometry args={[10, 16, 8]}/>*/}
+                    {/*</mesh>*/}
+                </pointLight>
+
+
+                <mesh
+                    position={[0, -200, 0]}
+                    scale={[10, 10, 10]}
+                    receiveShadow={true}
+                    castShadow={true}
+                >
+                    {ObjToPrimitive({url: "models/FinalBaseMesh.obj", mat})}
                 </mesh>
+            </group>
 
-                {/*<ambientLight color="white" intensity={0.1}/>*/}
-
-            </pointLight>
-
-            <mesh
-                position={[- windowWidth/5,-30,0]}
-                scale={[10,10,10]}
-                receiveShadows={true}
-            >
-                {ObjToPrimitive({ url: "models/FinalBaseMesh.obj", mat })}
-            </mesh>
 
         </>
     );
