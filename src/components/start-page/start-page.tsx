b7bbefae66a3fbbd6b2092021b53920452e3bc82
Giraphi <hoeps.raphael@gmail.com>
2021-08-13 01:10:45 +0200

    headline

--- src/components/start-page/start-page.tsx
+++ src/components/start-page/start-page.tsx
@@ -4,16 +4,50 @@ import StartPageWorld from "./start-page-world";
 import CameraControlButtons from "../shared/camera-control-buttons";
 import styled from "styled-components";
 import {motion} from "framer-motion";
+import {breakpointSmall} from "../../style/constants";
 
 const StyledRoot = styled(motion.div)`
     height: 100vh;
     background-color: black;
     padding-bottom: 70px;
+    position: relative;
     
     @media (min-width: 768px) {
         padding-bottom: 0;
     }
 `
+
+const StyledHeadlineContainer = styled.div`
+    position: absolute;
+    top: 0;
+    color: white;
+    z-index: 1;
+`
+
+const StyledHeadline = styled.div`
+    margin-bottom: 0;
+    
+    font-size: 40px;
+    
+    @media(min-width: ${breakpointSmall}) {        
+        font-size: 70px;
+    }
+`
+
+const StyledSubHeadline = styled.div`
+    font-size: 17px;
+    letter-spacing: 13px;
+    margin-top: -10px;
+    margin-left: 3px;
+
+    @media(min-width: ${breakpointSmall}) {
+        font-size: 30px;
+        letter-spacing: 22px;
+        margin-top: -20px;
+        margin-left: 6px;
+    }
+`
+
 
 export default function StartPage() {
     useEffect(() => {
@@ -28,6 +62,14 @@ export default function StartPage() {
             exit={{opacity: 0}}
             transition={{duration: 1.0}}
         >
+            <StyledHeadlineContainer>
+                <StyledHeadline>
+                    RAPHAEL HÖPS
+                </StyledHeadline>
+                <StyledSubHeadline>
+                    Web Developer
+                </StyledSubHeadline>
+            </StyledHeadlineContainer>
                 <ThreeBaseline
                     color={"black"}
                     controlButtons={<CameraControlButtons/>}
--- src/style/global-style.tsx
+++ src/style/global-style.tsx
@@ -1,41 +1,45 @@
 import {createGlobalStyle} from "styled-components";
 import {breakpointSmall} from "./constants";
+// import AuvantGothic from "../fonts/OPTIAuvantGothic-Bold.woff";
 
 const GlobalStyle = createGlobalStyle`
     html,
     body,
     #root {
-      margin: 0;
-      padding: 0;
+        margin: 0;
+        padding: 0;
     }
 
     html {
-      box-sizing: border-box;
-    }
+        box-sizing: border-box;
+        font-family: "AuvantGothicBold", sans-serif;
+    }   
+
     *, *:before, *:after {
-      box-sizing: inherit;
+        box-sizing: inherit;
     }
+
     h1 {
         margin-top: 0;
     }
-    
+
     body.is-start-page {
         overflow: hidden;
     }
-    
+
     body {
         font-size: 20px;
-        
+
         h1 {
             font-size: 30px;
             margin-bottom: 10px;
         }
     }
-    
+
     @media (min-width: ${breakpointSmall}) {
         body {
             font-size: 4vh;
-            
+
             h1 {
                 font-size: 8vh;
                 margin-bottom: 2vh;
