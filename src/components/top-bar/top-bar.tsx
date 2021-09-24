a34b2680d7082126cfb8ddfd72f9fbfcc9404b0f
Giraphi <hoeps.raphael@gmail.com>
2021-09-24 23:46:32 +0200

    menu

--- src/components/top-bar/top-bar.tsx
+++ src/components/top-bar/top-bar.tsx
@@ -13,6 +13,10 @@ const ButtonSizePx = 60;
 const BarHeightPx = 0.08 * ButtonSizePx;
 const BarSpacePx = 0.07 * ButtonSizePx;
 
+const ButtonSizeSmPx = 45;
+const BarHeightSmPx = 0.08 * ButtonSizeSmPx;
+const BarSpaceSmPx = 0.07 * ButtonSizeSmPx;
+
 const flickerAnimationMixin = css`
     animation-timing-function: step-end;
     animation-duration: ${flickerAnimationDurationMs}ms;
@@ -24,59 +28,85 @@ const StyledTop = styled.div<{ isHidden: boolean }>`
     top: 0;
     right: 0;
     z-index: ${zIndexes.topBarTop};
-    opacity: 1;
-    transition: opacity 0.2s linear;
     display: flex;
     cursor: pointer;
     mix-blend-mode: difference;
+    
+    ${props => props.isHidden && css`
+        display: none;
+    `}
 `;
 
 const StyledBar = styled.div`
-    height: ${BarHeightPx}px;
+    height: ${BarHeightSmPx}px;
     margin-left: 20%;
     margin-right: 20%;
     background-color: black;
     transition: transform 0.4s ease-in;
+
+    @media (min-width: ${breakpointSmall}) {
+        height: ${BarHeightPx}px;
+    }
 `;
 
 const StyledButton = styled.div<{ isMenuOpen: boolean }>`
-    width: ${ButtonSizePx}px;
-    height: ${ButtonSizePx}px;
+    width: ${ButtonSizeSmPx}px;
+    height: ${ButtonSizeSmPx}px;
     display: flex;
     flex-direction: column;
     justify-content: center;
     background-color: white;
 
     ${StyledBar}:first-child {
-        margin-bottom: ${BarSpacePx}px;
+        margin-bottom: ${BarSpaceSmPx}px;
+    }
+
+    @media (min-width: ${breakpointSmall}) {
+        width: ${ButtonSizePx}px;
+        height: ${ButtonSizePx}px;
+        ${StyledBar}:first-child {
+            margin-bottom: ${BarSpacePx}px;
+        }
     }
 
     ${props => props.isMenuOpen && css`
         ${StyledBar}:first-child {
-            transform: translateY(calc((${BarHeightPx}px + ${BarSpacePx}px) / 2)) rotate(315deg);
+            transform: translateY(calc((${BarHeightSmPx}px + ${BarSpaceSmPx}px) / 2)) rotate(315deg);
         }
 
         ${StyledBar}:nth-child(2) {
-            transform: translateY(calc(-1 * (${BarHeightPx}px + ${BarSpacePx}px) / 2)) rotate(-315deg);
+            transform: translateY(calc(-1 * (${BarHeightSmPx}px + ${BarSpaceSmPx}px) / 2)) rotate(-315deg);
+        }
+
+        @media (min-width: ${breakpointSmall}) {
+            ${StyledBar}:first-child {
+                transform: translateY(calc((${BarHeightPx}px + ${BarSpacePx}px) / 2)) rotate(315deg);
+            }
+
+            ${StyledBar}:nth-child(2) {
+                transform: translateY(calc(-1 * (${BarHeightPx}px + ${BarSpacePx}px) / 2)) rotate(-315deg);
+            }
         }
     `}
+
+
 `
 
 const StyledMenu = styled.div<{ isMenuOpen: boolean }>`
     width: 100vw;
     height: 100vh;
-    
+
     position: fixed;
     top: 0;
     z-index: ${zIndexes.topBarMenu};
-    
+
     visibility: hidden;
     background-color: black;
 
     ${props => !props.isMenuOpen && css`
         animation-timing-function: ease-in;
         animation-name: ${hideMenuKeyframes};
-        animation-duration: 400ms;
+        animation-duration: 500ms;
         animation-fill-mode: forwards;
     `}
 
@@ -85,25 +115,25 @@ const StyledMenu = styled.div<{ isMenuOpen: boolean }>`
         animation-name: ${showMenuKeyframes};
         animation-duration: 400ms;
         animation-fill-mode: forwards;
-    `}    
+    `}
 `
 
-const StyledLink = styled(Link)<{$isActive: boolean}>`
+const StyledLink = styled(Link)<{ $isActive: boolean }>`
     font-size: 30px;
     margin-bottom: 20px;
     ${flickerAnimationMixin};
     animation-name: ${whiteToBlackColorKeyframes};
     text-decoration: none;
-    
-    @media(min-width: ${breakpointSmall}) {
+
+    @media (min-width: ${breakpointSmall}) {
         font-size: 8vh;
         margin-bottom: 2vh;
     }
-    
+
     &:hover {
         color: red !important;
     }
-    
+
     ${props => props.$isActive && css`
         color: red !important;
         cursor: default;
@@ -116,7 +146,7 @@ const StyledInnerMenu = styled.div`
     height: 100%;
     ${flickerAnimationMixin};
     animation-name: ${blackToWhiteBackgroundKeyframes};
-    
+
     display: flex;
     flex-direction: column;
     justify-content: center;
