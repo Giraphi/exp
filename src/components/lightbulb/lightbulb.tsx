d3c3c9260dbe5b72e5f210a3fe9912a9ddd725fb
Giraphi <hoeps.raphael@gmail.com>
2021-10-04 20:03:38 +0200

    WIP refactor lights

---
+++ src/components/lightbulb/lightbulb.tsx
@@ -0,0 +1,163 @@
+import React, {useContext, useEffect, useMemo, useRef, useState} from "react";
+import {
+    Color,
+    Group,
+} from "three";
+import {Vector3} from "three/src/math/Vector3";
+import {HistoryContext} from "../../contexts/history-context";
+import {Text} from "@react-three/drei";
+import auvantGothic from "../../fonts/OPTIAuvantGothic-Bold.woff";
+import LightbulbLights from "./lightbulb-lights";
+
+const LOW_LIGHT_OFFSET = 30;
+
+const COLORS = {
+    mesh: "#ffffff",
+    light: "#ffffff"
+}
+
+const COLORS_NEGATIVE = {
+    mesh: "#000000",
+    light: "#ffffff"
+}
+
+const COLORS_HOVER = {
+    mesh: "#FF0500",
+    light: "#FF0000",
+}
+
+export interface LightParams {
+    intensity: number;
+    distance: number;
+    decay: number;
+}
+
+export interface LightbulbProps {
+    position: Vector3;
+    text: string;
+    height: number;
+    onClick: () => void;
+    path: string;
+    isActive?: boolean;
+    horizontal?: boolean;
+    lightParams?: { inner?: Partial<LightParams>, outer?: Partial<LightParams> }
+    negative?: boolean;
+}
+
+export default function Lightbulb(props: LightbulbProps) {
+    const [colors, setColors] = useState(props.negative ? COLORS_NEGATIVE : COLORS);
+    const groupRef = useRef<Group>(null);
+    const lightPositionY = props.height * 0.8;
+    const [isClicked, setIsClicked] = useState(false);
+    const history = useContext(HistoryContext).history;
+
+    useEffect(() => {
+        return () => {document.body.style.cursor = ""};
+    }, [])
+
+    useEffect(() => {
+        if (props.isActive) {
+            setColors(COLORS_HOVER);
+            return
+        }
+        props.negative ? setColors(COLORS_NEGATIVE) : setColors(COLORS);
+    }, [props.isActive, props.negative])
+
+    const lowLightPosition = useMemo(() => {
+        const lightPosition = new Vector3();
+        lightPosition.copy(props.position);
+        lightPosition.add(new Vector3(0, LOW_LIGHT_OFFSET, 0));
+
+        return lightPosition;
+    }, [props.position]);
+
+    function onPointerOver() {
+        if (props.isActive) {
+            return;
+        }
+        setColors(COLORS_HOVER);
+        document.body.style.cursor = "pointer";
+    }
+
+    function onPointerOut() {
+        document.body.style.cursor = "";
+        if (props.isActive) {
+            return;
+        }
+
+        if (isClicked) {
+            return;
+        }
+        setColors(props.negative ? COLORS_NEGATIVE : COLORS);
+    }
+
+    function onClick() {
+        if (props.isActive) {
+            return;
+        }
+
+        setColors(COLORS_HOVER);
+        setIsClicked(isClicked => !isClicked);
+        props.onClick();
+        setTimeout(() => {
+            history.push(props.path);
+        }, 1000);
+    }
+
+    // const position = useMemo(() => {
+    //     console.log("test22");
+    //     return new Vector3(0, lightPositionY - LOW_LIGHT_OFFSET, 0);
+    // }, [lightPositionY]);
+
+    return (
+        <group
+            position={lowLightPosition}
+            rotation={props.horizontal ? [0, 0, Math.PI / 2] : undefined}
+        >
+            <LightbulbLights
+                lightPositionY={lightPositionY}
+                lightParams={props.lightParams}
+                color={colors.light}
+                position={props.position}
+                horizontal={!!props.horizontal}
+            >
+                    <group
+                        ref={groupRef}
+                        position={[0, -lightPositionY - 10, 0]}
+                    >
+                        <mesh
+                            scale={[20, props.height, 20]}
+                            position={[0, props.height / 2, 0]}
+                            onPointerOver={onPointerOver}
+                            onClick={onClick}
+                            onPointerOut={onPointerOut}
+                        >
+                            <meshStandardMaterial
+                                emissive={new Color(colors.mesh)}
+                                emissiveIntensity={1}
+                                color={"#000000"}
+                            />
+
+                            <boxBufferGeometry
+                                args={[1, 1, 1]}/>
+                        </mesh>
+
+                        <Text
+                            fontSize={15}
+                            lineHeight={1}
+                            font={auvantGothic}
+                            color={props.negative ? "white" : "black"}
+                            position={[0,props.height - 5,12]}
+                            rotation={[0, 0, -Math.PI / 2]}
+                            anchorX={"left"}
+                            anchorY={"middle"}
+                        >
+                            {props.text}
+                        </Text>
+                    </group>
+
+            </LightbulbLights>
+
+        </group>
+    );
+}
